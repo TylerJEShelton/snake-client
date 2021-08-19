1190f4ba2663d5a1a0db1f783916ac32a79ffe48
Tyler <tylershelton99@gmail.com>
2021-08-19 01:12:45 +0000

    Completed Stdin Setup Assignment where we set up the ability to input keyboard commands for movements

--- play.js
+++ play.js
@@ -1,4 +1,22 @@
 const connect = require("./client");
 
+const setupInput = () => {
+  const stdin = process.stdin;
+  const handleUserInput = () => {
+    stdin.on("data", (key) => {
+      if (key === '\u0003') {
+        process.exit();
+      }
+    });
+  };
+
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+};
+
 console.log("Connecting ...");
 connect();
+setupInput();
