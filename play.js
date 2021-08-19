fe28f63ae2a9456e91e2afaf655aba4ee56254b3
Tyler <tylershelton99@gmail.com>
2021-08-19 02:45:44 +0000

    Completed Input Module Challenge where we refactored play to separate out input code

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
 const connect = require("./client");
-
-const setupInput = () => {
-  const stdin = process.stdin;
-  const handleUserInput = () => {
-    stdin.on("data", (key) => {
-      if (key === '\u0003') {
-        process.exit();
-      }
-    });
-  };
-
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  handleUserInput();
-  return stdin;
-};
+const setupInput = require("./input");
 
 console.log("Connecting ...");
 connect();
